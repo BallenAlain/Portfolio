ea018675fa6c877eb4df04391244ad53b7e74b40
Alain Ballen <alainballen@gmail.com>
2025-01-31 02:03:54 -0500

    about page

---
+++ src/components/about/AboutLink.vue
@@ -0,0 +1,67 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'AboutLinks',
+    props: {
+        text: {
+            type: String,
+            required: true,
+            default: "n/a",
+        },
+        name: {
+            type: String,
+            required: true,
+            default: 'n/a',
+        },
+        link: {
+            type: String,
+            required: true,
+            default: 'n/a',
+        }
+    }
+})
+</script>
+
+<template>
+    <span class="container">
+        {{ text }}
+    <a :href="link" target="_blank"><span class="link-name"> {{ name }} <v-icon name="hi-external-link" /></span>
+        &nbsp;
+    </a>
+</span>
+</template>
+
+<style scoped>
+.container {
+    white-space: nowrap;
+}
+
+.link-name {
+    color: var(--vt-c-black-soft);
+    position: relative;
+    transition: transform 0.3s ease;
+    padding-bottom: 3px;
+    font-size: clamp(0.2rem, 2vw, 1rem);
+    background-image: linear-gradient(to right, var(--text-dark-soft) 0%, var(--text-dark-soft) 100%);
+    background-size: 0% 2px;
+    background-position: left bottom;
+    background-repeat: no-repeat;
+    transition: background-size 0.3s ease-in-out, padding-left 0.5s ease;
+}
+
+.link-name:hover {
+    background-size: 100% 2px;
+    padding-left: 10px;
+}
+
+a {
+    text-decoration: none;
+}
+
+@media (max-width: 720px) {
+    .container {
+    white-space: normal;
+}
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,25 +1,54 @@
+<script setup lang="ts">
+import jsonData from '@/data/about-links.json'
+import AboutLink from '@/components/about/AboutLink.vue';
+
+const items = jsonData
+</script>
+
 <template>
   <div class="about">
     <div>
       <h1>Meet <span class="name">Alain</span></h1>
       <div class="about-content">
-        <p>coding shizz</p>
-        <p>check out my resume!</p>
-      <br>
-      <p>Beyond the lines of code, <span class="name">catch me: </span>
+        <p>I am a recent Software Engineering graduate from York University, eager to start a career in software
+          development.
+          With a strong foundation in backend development, I honed my skills through hands-on experience in
+          building mobile applications,
+          interactive dashboards, and tools leveraging Large Language Models (LLMs). Passionate about creating secure,
+          scalable, and impactful software,
+          I thrive on simplifying common workflows and delivering solutions that make an impact.</p><br>
+        <p>While I excel in backend development, I am also eager about exploring full-stack development and
+          expanding my expertise. Additionally,
+          I am passionate by the world of AI, particularly the applications of LLMs and their potential to transform
+          industries.</p><br>
+        <p>Driven by curiosity and a commitment to continuous learning, I actively seek new graduate roles
+          where I can contribute to
+          innovative projects, grow as a developer, and make a meaningful impact in the tech industry.</p>
+        <br>
+        <br>
+        <p class="connect">Lets connect!</p>
+        <div class="about-link-container">
+          <AboutLink v-for="item in items"
+          :key="item.link"
+          :text="item.text" 
+          :name="item.name" 
+          :link="item.link" />
+        </div>
+        <br><br><hr><br>
+        <p>Beyond the lines of code, <span class="name">catch me: </span>
         <ul>
           <li>In the gym 🏋️</li>
           <li>Going out for a paddle on an outrigger canoe 🛶</li>
           <li>Training hard on the water for dragon boat racing 🐲</li>
-          <li>Trying out new food (brand ambassador for @torontomunching) 🍣</li>
+          <li>Trying out new food (brand ambassador @torontomunching) 🍣</li>
           <li>Exploring new places! ✈️</li>
         </ul>
         </p>
       </div>
     </div>
-    
-    <img src="../assets/myself.png" width="601" height="967">
-    
+
+    <img class="image" src="../assets/myself.png" width="301" height="460">
+
   </div>
 </template>
 
@@ -37,7 +66,40 @@
   font-size: 3rem;
 }
 
+.connect {
+  font-weight: bold;
+  font-size: 30px;
+}
+
 .about-content {
   white-space: wrap;
 }
+
+.about-content a {
+  text-decoration: none;
+}
+
+.about-link-container{
+  display: flex;
+  flex-wrap: wrap;
+}
+
+@media (max-width: 720px) {
+  .about {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+  }
+  .about-link-container{
+  display: inline-block;
+  flex-wrap: wrap;
+  gap:10vw;
+}
+.image{
+  order:-1;
+  
+}
+}
+
+
 </style>
