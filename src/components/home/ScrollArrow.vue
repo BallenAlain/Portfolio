0343eb789237025eda5cfc74d53070a42b0e7b46
Alain Ballen <alainballen@gmail.com>
2025-02-01 00:08:37 -0500

    scroll arrow animation

--- src/components/home/ScrollArrow.vue
+++ src/components/home/ScrollArrow.vue
@@ -1,7 +1,19 @@
 <template>
-  <i class="arrow down"></i>
+  <div class="container">
+    <i class="arrow down"></i>
+  </div>
 </template>
-<style>
+<style scoped>
+.container {
+  transition: transform 0.3s ease;
+  display: inline-block; 
+  line-height: 0;
+  padding: 0;
+  margin: 0;
+}
+.container:hover {
+  transform: translateY(10px);
+}
 .arrow {
   border: solid var(--alainblue);
   border-width: 0 3px 3px 0;
